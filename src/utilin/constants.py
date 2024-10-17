bce7eb5b7bead00d96dd39c5595e1c81d576d050
Floris vanderFlier <floris.vanderflier@iff.com>
2024-10-17 12:00:43 +0200

    feat: added tests and github action for setup and testing

--- src/utilin/constants.py
+++ src/utilin/constants.py
@@ -1,2 +1,49 @@
 AA_ALPHABET = list("ACDEFGHIKLMNPQRSTVWY")
 AA_ALPHABET_GREMLIN = list("ARNDCQEGHILKMFPSTWYV-")
+THREE_TO_SINGLE_LETTER_CODES = {
+    "ALA": "A",
+    "ARG": "R",
+    "ASN": "N",
+    "ASP": "D",
+    "CYS": "C",
+    "GLN": "Q",
+    "GLU": "E",
+    "GLY": "G",
+    "HIS": "H",
+    "ILE": "I",
+    "LEU": "L",
+    "LYS": "K",
+    "MET": "M",
+    "PHE": "F",
+    "PRO": "P",
+    "SER": "S",
+    "THR": "T",
+    "TRP": "W",
+    "TYR": "Y",
+    "VAL": "V",
+}
+SINGLE_TO_THREE_LETTER_CODES = {
+    "A": "ALA",
+    "R": "ARG",
+    "N": "ASN",
+    "D": "ASP",
+    "C": "CYS",
+    "Q": "GLN",
+    "E": "GLU",
+    "G": "GLY",
+    "H": "HIS",
+    "I": "ILE",
+    "L": "LEU",
+    "K": "LYS",
+    "M": "MET",
+    "F": "PHE",
+    "P": "PRO",
+    "S": "SER",
+    "T": "THR",
+    "W": "TRP",
+    "Y": "TYR",
+    "V": "VAL",
+}
+UNIPROT_ACCESSION_PATTERN = (
+    r"[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}"
+)
--- src/utilin/plotting/prediction.py
+++ src/utilin/plotting/prediction.py
@@ -15,15 +15,22 @@ def plot_prediction_scatter(
     y_label: str = "Prediction",
     title: Optional[str] = None,
     axis_label_fontsize: int = 16,
+    title_fontsize: int = 18,
+    margin_ratio: float = 0.15,
 ) -> Tuple[Figure, Axes]:
+    x_min, x_max = min(y_true), max(y_true)
+    y_min, y_max = min(y_pred), max(y_pred)
+    x_margin = margin_ratio * (x_max - x_min)
+    y_margin = margin_ratio * (y_max - y_min)
     fig, ax = plt.subplots()
     ax.scatter(y_true, y_pred, c="k", s=50, alpha=0.75)
     ax.set_xlabel(x_label, fontsize=axis_label_fontsize)
     ax.set_ylabel(y_label, fontsize=axis_label_fontsize)
-    ax.set_title(title)
+    ax.set_title(title, fontsize=title_fontsize)
+    ax.set_xlim(x_min, x_max + x_margin)
+    ax.set_ylim(y_min, y_max + y_margin)
     spearman, _ = spearmanr(y_true, y_pred)
     r2 = r2_score(y_true, y_pred)
-    print(spearman, r2)
     metrics_str = f"Spearman: {spearman:.2f}\nR2: {r2:.2f}"
     box_format = dict(boxstyle="round", facecolor="white", alpha=0.5)
     ax.text(
@@ -31,7 +38,7 @@ def plot_prediction_scatter(
         0.95,
         metrics_str,
         transform=ax.transAxes,
-        fontsize=14,
+        fontsize=12,
         verticalalignment="top",
         bbox=box_format,
     )
