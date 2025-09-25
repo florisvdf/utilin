3d90679570a6acbc9b64d38a4cae6d3fc8b5ca46
Floris vanderFlier <floris.vanderflier@iff.com>
2025-09-25 13:42:03 +0200

    feat: added msa reformatting with reformat.pl

--- src/utilin/utils.py
+++ src/utilin/utils.py
@@ -2,8 +2,12 @@ import requests
 from pathlib import Path
 from collections import defaultdict, Counter
 from typing import Union, Dict, List
+import itertools
+import subprocess
+import shutil
 
 import numpy as np
+import pandas as pd
 
 from biotite.sequence.io.fasta import FastaFile
 
@@ -61,6 +65,17 @@ def variant_sequence_to_mutations(variant: str, reference: str) -> str:
             if aa_ref != aa_var
         ]
     )
+
+
+def variant_sequence_to_positions(
+    variant: str, reference: str, indexing: str = "one"
+) -> list:
+    offset = 0 if indexing == "zero" else 1
+    return [
+        pos + offset
+        for pos, (aa_ref, aa_var) in enumerate(zip(reference, variant))
+        if aa_ref != aa_var
+    ]
 
 
 def mutations_to_sequence(mutations: List[str], reference: str) -> str:
@@ -126,3 +141,70 @@ def group_position_non_overlapping(combinations: List[tuple]) -> Dict[tuple, int
             new_grouping.pop(old_set, None)
         new_grouping[new_combination] = counts
     return new_grouping
+
+
+def variants_to_position_sets(
+    sequences: Union[pd.Series, List[str]], reference: str, indexing: str = "one"
+) -> List[list]:
+    position_sets = list(
+        map(lambda x: variant_sequence_to_positions(x, reference, indexing), sequences)
+    )
+    return position_sets
+
+
+def variants_to_median_mutations(
+    sequences: Union[pd.Series, List[str]], reference: str
+) -> int:
+    position_sets = variants_to_position_sets(sequences, reference)
+    set_sizes = list(map(lambda x: len(x), position_sets))
+    return np.median(set_sizes)
+
+
+def variants_to_max_mutations(
+    sequences: Union[pd.Series, List[str]], reference: str
+) -> int:
+    position_sets = variants_to_position_sets(sequences, reference)
+    set_sizes = list(map(lambda x: len(x), position_sets))
+    return np.max(set_sizes)
+
+
+def variants_to_position_coverage(
+    sequences: Union[pd.Series, List[str]], reference: str
+) -> float:
+    position_sets = variants_to_position_sets(sequences, reference)
+    total_mutated_positions = len(set(itertools.chain.from_iterable(position_sets)))
+    coverage = total_mutated_positions / len(reference)
+    return coverage
+
+
+def variants_to_unique_position_combinations(
+    sequences: Union[pd.Series, List[str]], reference: str
+) -> int:
+    position_sets = variants_to_position_sets(sequences, reference)
+    n_unique_sets = len(set(map(lambda x: tuple(x), position_sets)))
+    return n_unique_sets
+
+
+def variants_to_position_redundancy(
+    sequences: Union[pd.Series, List[str]], reference: str
+) -> float:
+    position_sets = variants_to_position_sets(sequences, reference)
+    all_mutated_positions = itertools.chain.from_iterable(position_sets)
+    position_counts = Counter(all_mutated_positions)
+    position_frequencies = [
+        position_counts[pos] / len(sequences)
+        for pos in range(1, len(position_sets) + 1)
+    ]
+    return sum(position_frequencies) / len(reference)
+
+
+def reformat_msa(
+    input_path: str, output_path: str, input_format: str, output_format: str
+) -> str:
+    """
+    Usage: reformat.pl [informat] [outformat] infile outfile [options]
+    """
+    if shutil.which("reformat.pl") is None:
+        raise RuntimeError("reformat.pl not found in path!")
+    command = ["reformat.pl", input_format, output_format, input_path, output_path]
+    subprocess.run(command)
